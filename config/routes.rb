602db790da177db60bcff5b0116e2c6afb28735f
Wvertos <wvertos@gmail.com>
2022-09-29 14:49:36 +0200

    create comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
